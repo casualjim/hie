51056eb119703eb7d24a81a7607cbcf911e72c87
Ivan Porto Carrero <ivan@flanders.co.nz>
2022-10-29 21:48:29 -0700

    make find use AsIter

--- filter.go
+++ filter.go
@@ -1,9 +1,10 @@
 package hie
 
 // Find returns the first match in the iterator, or none if it can't be found
-func Find[T any](iter Iter[T], predicate Predicate[T]) Option[T] {
-	for iter.HasNext() {
-		elem := iter.Next()
+func Find[T any](iter AsIter[T], predicate Predicate[T]) Option[T] {
+	i := iter.AsIter()
+	for i.HasNext() {
+		elem := i.Next()
 		if predicate(elem) {
 			return Some(elem)
 		}
